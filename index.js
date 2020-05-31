c3c7e3d456105d04ca24dcee9d77cf165165499e
fortunate-engine-7446 <brenande@gmail.com>
2020-05-31 21:14:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function logWhisper(string) {
 // }
 
 function SayHiToGrandma(string) {
-  if (string == "I love you, Grandma.") {
+  if (string = "I love you, Grandma.") {
     return "I love you, too."
   }
 }
