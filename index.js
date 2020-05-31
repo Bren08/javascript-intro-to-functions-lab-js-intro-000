552e29fc3dc2abfc17247436d4ff02a12a664fb8
fortunate-engine-7446 <brenande@gmail.com>
2020-05-31 23:45:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function shout(string) {
   return string.toUpperCase();
 };
-shout("grandma");
+
 
 function whisper(string) {
   return string.toLowerCase();
 };
-whisper("grandma")
+
 
 function logShout(string) {
   console.log(string.toUpperCase());
 };
-logShout("grandma")
+
 
 function logWhisper(string) {
   console.log(string.toLowerCase());
 };
-logWhisper("grandma")
+
 
 // function sayHiToGrandma(string) {
 //   if (string === string.toLowerCase());
