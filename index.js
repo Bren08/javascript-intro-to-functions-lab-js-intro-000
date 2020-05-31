6eb20f579d4713e62891341317b76ffbf6cf8979
fortunate-engine-7446 <brenande@gmail.com>
2020-05-31 23:44:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 };
+shout("grandma");
 
 function whisper(string) {
-  return string.toLowerCase()
-}
+  return string.toLowerCase();
+};
+whisper("grandma")
 
 function logShout(string) {
-  console.log(string.toUpperCase())
-}
+  console.log(string.toUpperCase());
+};
+logShout("grandma")
 
 function logWhisper(string) {
-  console.log(string.toLowerCase()
-}
-
+  console.log(string.toLowerCase());
+};
+logWhisper("grandma")
 
 // function sayHiToGrandma(string) {
-//   if (string === string.toLowerCase()) {
+//   if (string === string.toLowerCase());
 //   return "I can't hear you!";
-// } else if (string === "I love you, Grandma.") {
+//   else if (string === "I love you, Grandma.");
 //   return "I love you, too.";
-// } else if (string === string.toUpperCase()) {
+//   else if (string === string.toUpperCase());
 //   return "YES INDEED!";
-// }
-// }
+// };
