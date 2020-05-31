fb9ef84f325a5283328f2e80b6539e4fe7c04aa7
fortunate-engine-7446 <brenande@gmail.com>
2020-05-31 22:34:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var lowercase = "i cant hear you"
-  string.toLowerCase() === lowercase {
+  string.toLowerCase() === lowercase 
   return "I can't hear you!"
-}
+
 }
