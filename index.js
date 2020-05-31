25aeb379a660adde3eeef10c0eef4b1ee7caa12c
fortunate-engine-7446 <brenande@gmail.com>
2020-05-31 21:56:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,19 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "I can't hear you!"
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string == string.toUpperCase()) {
-    return "YES INDEED!"
-  }
-}
-
-function SayHiToGrandma(string) {
-  if (string == ("I love you, Grandma.")) {
-    return "I love you, too."
+  var hear = "I can't hear you!"
+  hear.toLowerCase() === hear {
+    return 'I can\'t hear you!'
   }
 }
